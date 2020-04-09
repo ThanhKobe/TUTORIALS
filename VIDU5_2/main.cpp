a575815513d3e63a15993aceea9fb55cf9fc99ad
ThanhKobe <quocthanh1205@gmail.com>
2020-04-09 14:51:57 +0700

    new 09-04

--- VIDU5_2/main.cpp
+++ VIDU5_2/main.cpp
@@ -1,47 +1,35 @@
 #include<iostream>
 #include<math.h>
-#include<iomanip> //std::setw
-
-void nhap(int a[],int &n);
-void xuat(int a[],int n);
-int nguyento(int n);
-int demsonguyento(int a[], int n);
-
-int main(){
-    int n,a[100];
+using namespace std;
+bool check(int n) {
+    if (n <= 1) return false;
+    for (int i = 2; i <= sqrt(n); i++)
+        if (n % i == 0)
+            return false;
+    return true;
+}
+int dem(int a[], int n) {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (check(a[i]) == true)
+            count++;
+    }
+    return count;
+}
+void nhap(int a[], int n) {
+    cout << "-----Nhap mang-----"<<endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Nhap phan tu a[" << i << "]: ";
+        cin >> a[i];
+    }
+}
+int main() {
+    int a[1000];
+    int n;
+    cout << "Nhap n: ";
+    cin >> n;
     nhap(a,n);
-    xuat(a,n);
-
-    std::cout<<" so luong cac nguyen to trong mang la: "<<  demsonguyento(a,n);
-
+    cout << "So luong cac so nguyen to la: " << dem(a,n)<<endl;
+    system("pause");
     return 0;
-}
-
-void nhap(int a[],int &n){
-    std::cin >> n;
-    for (int i=0; i<n; i++)
-        std::cin>>a[i];
-}
-
-void xuat(int a[],int n){
-    std::cout << std::endl;
-    for (int i=0; i<n; i++)
-    std::cout << a[i] << std::setw(4);
-}
-
-int nguyento(int n){
-    int d=0;
-    for (int i=0;i<=n;i++)
-        if(n%i==0)
-        d++;
-    
-    return(d==2);
-}
-
-int demsonguyento(int a[], int n){
-    int d=0;
-    for (int i=0;i<n;i++)
-        if(nguyento(a[i]))
-            d++;
-    return d;
 }
