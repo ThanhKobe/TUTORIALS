6521957130bccf2160d3c4fac51f6d0efed7dc26
ThanhKobe <quocthanh1205@gmail.com>
2020-04-11 20:06:54 +0700

    new 11-04

---
+++ VIDU6_4/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<iomanip>
+
+
+using namespace std;
+
+void nhap(int *a,int m, int n){
+    for(int i=0;i<m;i++)
+    for(int j=0;j<n;j++)
+    {
+        gotoxy
+    }
+}
---
+++ VIDU7_1/main.d
@@ -0,0 +1,1 @@
+obj/main.o: /home/thanhkobe/Documents/TUTORIALS/VIDU7_1/main.cpp
